626826da5c85cc9cad61726f4d4d8231e3e80804
Soggaling <soggal1ng@onionmail.org>
2026-04-12 23:50:13 +1200

    Update main.py

--- toolkit/main.py
+++ toolkit/main.py
@@ -17,19 +17,23 @@ def main_menu():
     while True:
         clear()
         banner()
-        print(" [1] IP Grabber")
+        print(" [1] IP Logger")
         print(" [2] MAC Spoofer")
-        print(" [3] Recon Kit")
+        print(" [3] WIFI Harvest")
         print(" [Q] Exit Toolkit")
         
         choice = input("\n>> ").lower()
 
         if choice == '1':
+            # Recommended to put the toolkit on a usb and run ip_grabber on the target PC
             from modules import ip_grabber
             ip_grabber.run()
         elif choice == '2':
             from modules import mac_spoofer
             mac_spoofer.run()
+        elif choice =='3':
+            # Same as choice one :3
+            from modules import wifi_harv
         elif choice == 'q':
             print("Shutting down...")
             break
